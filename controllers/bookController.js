106bbfcc825573897ddd80a620e030382e2ddb9d
Coaxial <thecoaxial@gmail.com>
2021-03-21 18:21:12 -0400

    changed db.Book to Book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,29 +1,29 @@
-const db = require("../models");
+const Book = require("../models");
 
 // Define controller Methods
 
 module.exports = {
     findAll : function(req, res) {
-        db.Book.find(req.query)
+        Book.find(req.query)
           .sort({ title: 1 })
           .then(bookModel => res.json(bookModel))
           .catch(err => res.status(422).json(err));
     },
 
     findById : function(req, res) {
-        db.Book.findById(req.params.id)
+        Book.findById(req.params.id)
           .then(bookModel => res.json(bookModel))
           .catch(err => res.status(422).json(err));
     },
 
     store : function(req,res) {
-        db.Book.create(req.body)
+        Book.create(req.body)
           .then(bookModel => res.json(bookModel))
           .catch(err => res.status(422).json(err));
     }, 
 
     delete : function(req,res) {
-        db.Book.findById({ _id: req.params.id })
+        Book.findById({ _id: req.params.id })
           .then(bookModel => bookModel.remove())
           .then(bookModel => res.json(bookModel))
           .catch(err => res.status(422).json(err));
