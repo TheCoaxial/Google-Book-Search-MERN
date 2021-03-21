a6b8eabf68a62199fbceebd3b74c67237c0b6c0c
Coaxial <thecoaxial@gmail.com>
2021-03-21 16:59:34 -0400

    Adding book model + model index

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Book = require("./book");
+
+Book = require("./book");
+
+module.exports = Book;
+
+// Since I only have a single model I don't think the index is strictly necessary
+// But if I ever add more models this will be useful + better to stick with
+// convention
