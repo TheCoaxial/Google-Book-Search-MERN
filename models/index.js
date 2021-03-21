600e5a659dc054e47f4b7a993f1a7fda32ff962a
Coaxial <thecoaxial@gmail.com>
2021-03-21 17:49:25 -0400

    updated comments

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ Book = require("./book");
 
 module.exports = Book;
 
-// Since I only have a single model I don't think the index is strictly necessary
+// Other than being able to just require the folder /models instead of /models/book.js,
+// since I only have a single model I don't think the index is strictly necessary
 // But if I ever add more models this will be useful + better to stick with
 // convention
