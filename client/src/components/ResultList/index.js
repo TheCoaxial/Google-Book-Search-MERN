82c4f6162aadc4a3b0d0ca969ebcda17652f95e6
Coaxial <thecoaxial@gmail.com>
2021-03-21 23:15:51 -0400

    adding result list component

---
+++ client/src/components/ResultList/index.js
@@ -0,0 +1,26 @@
+import React from  "react";
+
+
+function BookResults(props) {
+
+
+    const books = props.books.map((book) => 
+    
+    [
+        <li>
+            <div className="float-container">
+                <div className="float-child"><p>{book.items.volumeInfo.title}</p></div>
+                <div className="float-child"><p>{book.items.volumeInfo.authors[0]}</p></div>
+                <div className="float-child"><p>{book.items.volumeInfo.description}</p></div>
+                <div className="float-child"><p>{book.items.volumeInfo.imageLinks.smallThumbnail}</p></div>
+                <div className="float-child"><p>{book.items.volumeInfo.infoLinks}</p></div>
+            </div>
+        </li>
+    ]
+
+    );
+
+    return(
+        <ul className="lItem"> {books} </ul>
+    )
+}
