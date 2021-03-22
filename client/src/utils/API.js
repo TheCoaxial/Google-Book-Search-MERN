e225d012868ce32e8373be01202c35ba208eb8ef
Coaxial <thecoaxial@gmail.com>
2021-03-21 22:29:10 -0400

    added search function for google books

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+
 export default {
     // Get all books
     getBooks: function() {
@@ -19,5 +23,10 @@ export default {
     // Save book
     saveBook: function(BookData) {
         return axios.post("/books", BookData);
+    },
+
+    // search Google books
+    search: function(title) {
+        return axios.get(BASEURL + title); 
     }
 };
