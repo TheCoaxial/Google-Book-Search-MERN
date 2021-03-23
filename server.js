8398c8c1b4823ce3a92ef3ec82ffef169ab614a8
Coaxial <thecoaxial@gmail.com>
2021-03-23 00:22:19 -0400

    updating MONGODB URI

--- server.js
+++ server.js
@@ -18,7 +18,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to Mongo
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactbooksearch");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactbooksearch",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
